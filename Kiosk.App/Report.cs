f66f58a0013d0cfd2f9f038bd091710e2820301b
ealigholi <e.aligholi@gmail.com>
2023-01-17 14:38:19 +0000

    first

--- Kiosk.App/Report.cs
+++ Kiosk.App/Report.cs
@@ -2,11 +2,18 @@ using System;
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
-
+using Aspose.Cells;
 namespace Kiosk.App;
+using Newtonsoft.Json;
 
 public class Report {
     public void Run() {
-        Console.WriteLine("Report");
+        var dummyJson = new {
+            title="question 1", answer_1= "YES", answer_2= "NO"
+        };
+
+        string jsondata=JsonConvert.SerializeObject(dummyJson);
+        var workbook = new Workbook(jsondata);
+        workbook.Save("s.html");
     }
 }
