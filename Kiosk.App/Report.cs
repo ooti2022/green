19520e136b64ef84da61328b7a586f557f01f759
ealigholi <e.aligholi@gmail.com>
2023-01-17 15:18:30 +0000

    now it works

--- Kiosk.App/Report.cs
+++ Kiosk.App/Report.cs
@@ -4,16 +4,21 @@ using System.Collections.Generic;
 using System.Linq;
 using Aspose.Cells;
 namespace Kiosk.App;
-using Newtonsoft.Json;
+using Newtonsoft.Json; 
 
 public class Report {
     public void Run() {
+        
         var dummyJson = new {
             title="question 1", answer_1= "YES", answer_2= "NO"
         };
 
-        string jsondata=JsonConvert.SerializeObject(dummyJson);
-        Console.WriteLine("Dotnet worksdsddddd!");
+        string jsondata = JsonConvert.SerializeObject(dummyJson);
+
+           
+         //var workbook = new Workbook(jsondata);
+        // workbook.Save("Output.html");
+
         // var workbook = new Workbook(jsondata);
         // workbook.Save("s.html");
     }
