9349ece33ed9d767e7c3ab25d1c801323c0eadc9
Osama Ahmed Tahir <osamaahmedtahir17@gmail.com>
2023-01-17 15:10:26 +0000

    File added

--- Kiosk.App/Results.cs
+++ Kiosk.App/Results.cs
@@ -4,15 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 namespace Kiosk.App;
 using Newtonsoft.Json;
+
 class Results {
     public void Run() {
-        //var q = new JsonSerializer();
          var dummyJson = new {
             title="question 1", answer_1= "YES", answer_2= "NO"
         };
-
         string jsondata=JsonConvert.SerializeObject(dummyJson);
-        Console.WriteLine("Dotnet worksdsddddd!");
-        Console.WriteLine("Results");
+        File.WriteAllText(@"path.txt", jsondata);
     }
 }
