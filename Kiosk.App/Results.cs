6fe0d3eb36efac63b9ee9ee7d77fa573d1de5693
Osama Ahmed Tahir <osamaahmedtahir17@gmail.com>
2023-01-17 15:11:49 +0000

    Numbers added

--- Kiosk.App/Results.cs
+++ Kiosk.App/Results.cs
@@ -8,7 +8,7 @@ using Newtonsoft.Json;
 class Results {
     public void Run() {
          var dummyJson = new {
-            title="question 1", answer_1= "YES", answer_2= "NO"
+            title="question 1", answer_1= "YES", number_1= 2, answer_2= "NO", number_2= 2, 
         };
         string jsondata=JsonConvert.SerializeObject(dummyJson);
         File.WriteAllText(@"path.txt", jsondata);
