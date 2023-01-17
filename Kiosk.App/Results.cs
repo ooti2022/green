c7e7a174139abda5ca218d8fe61a0bb7a18c11e7
Giusi <giusidipaolo@gmail.com>
2023-01-17 15:31:34 +0000

    added dummy method getResults

--- Kiosk.App/Results.cs
+++ Kiosk.App/Results.cs
@@ -7,10 +7,14 @@ using Newtonsoft.Json;
 
 class Results {
     public void Run() {
-         var dummyJson = new {
+            var dummyJson = new {
             title="question 1", answer_1= "YES", number_1= 2, answer_2= "NO", number_2= 2, 
-        };
-        string jsondata=JsonConvert.SerializeObject(dummyJson);
-        File.WriteAllText(@"path.txt", jsondata);
+            };
+            string jsondata=JsonConvert.SerializeObject(dummyJson);
+            File.WriteAllText(@"path.txt", jsondata);
+        }
+        
+    public void getResults(){
+        
     }
 }
