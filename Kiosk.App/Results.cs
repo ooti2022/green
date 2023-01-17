d1318c0b614f6249c7ddd7fbca8c13618bad87f5
Giusi <giusidipaolo@gmail.com>
2023-01-17 14:46:04 +0000

    json serializer

--- Kiosk.App/Results.cs
+++ Kiosk.App/Results.cs
@@ -2,11 +2,13 @@ using System;
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
-
+using System.Text.Json;
+using System.Text.Json.Serialization;
 namespace Kiosk.App;
 
 class Results {
     public void Run() {
+        //var q = new JsonSerializer();
         Console.WriteLine("Results");
     }
 }
